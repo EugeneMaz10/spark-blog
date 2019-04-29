eee6741ff0365eed11b241fad47d5b9a9516dd24
victor njonge <vinge1718@gmail.com>
2019-04-29 16:38:24 +0300

    added the dynamic routing in order to help view the individual views depending on the post item selected

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,12 +11,14 @@ public class App {
     public static void main(String[] args) { //type “psvm + tab” to autocreate this :)
         staticFileLocation("/public");
 
-        get("/", (request, response) -> {
+        //get: show new post form
+
+        get("/posts/new", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
-            ArrayList<Post> posts = Post.getAll();
-            model.put("posts", posts);
-            return new ModelAndView(model, "index.hbs");
+            return new ModelAndView(model, "newpost-form.hbs");
         }, new HandlebarsTemplateEngine());
+
+        //post: process new post form
 
         post("/posts/new", (request, response) ->{
             Map<String, Object> model = new HashMap<>();
@@ -24,5 +26,33 @@ public class App {
             Post newPost = new Post(content);
             return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
+
+        //get: show all posts
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Post> posts = Post.getAll();
+            model.put("posts", posts);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //get: show an individual post
+
+        get("/posts/:id", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfPostToFind = Integer.parseInt(request.params("id")); //pull id - must match route segment
+            Post foundPost = Post.findById(idOfPostToFind);    //use it to find post
+            model.put("post", foundPost);   //add it to model for template to display
+            return new ModelAndView(model, "post-detail.hbs");  //individual post page.
+        }, new HandlebarsTemplateEngine());
+
+        //get: show a form to update a post
+
+        //post: process a form to update a post
+
+        //get: delete an individual post
+
+        //get: delete all posts
+
     }
 }
